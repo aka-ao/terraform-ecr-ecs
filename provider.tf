7190f2f6d3a18010da6571d69bb86ffd3274df9d
赤堀蒼磨 <akahori-soma@akahori-soma-m1.local>
2021-05-01 22:27:51 +0900

    feat ECS起動に必要なロールを付与

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 }
 
 resource "aws_ecr_repository" "nginx" {
-  name = "my-nginx"
+  name                 = "my-nginx"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
@@ -17,39 +17,62 @@ output "ecr_url" {
   value = aws_ecr_repository.nginx.repository_url
 }
 
-data "aws_iam_role" "executionRole"{
-  name = "ecsTaskExecutionRole"
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "MyEcsTaskRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cloud_watch_agent_server_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
 resource "aws_ecs_task_definition" "task" {
-  cpu = "256"
+  cpu    = "256"
   memory = "512"
 
-  network_mode = "awsvpc"
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn = data.aws_iam_role.executionRole.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
   container_definitions = jsonencode([
     {
-      logConfiguration: {
-        logDriver: "awslogs",
-        options: {
-          awslogs-group: "/ecs/my-nginx",
-          awslogs-region: "ap-northeast-1",
-          awslogs-stream-prefix: "ecs"
+      logConfiguration : {
+        logDriver : "awslogs",
+        options : {
+          awslogs-group : "/ecs/my-nginx",
+          awslogs-region : "ap-northeast-1",
+          awslogs-stream-prefix : "ecs"
         }
       },
-      portMappings: [
+      portMappings : [
         {
-          hostPort: 80,
-          protocol: "tcp",
-          containerPort: 80
+          hostPort : 80,
+          protocol : "tcp",
+          containerPort : 80
         }
       ],
-      cpu: 256,
-      memoryReservation: 512,
-      essential: true,
-      name: "my-nginx",
-      image: "${aws_ecr_repository.nginx.repository_url}:latest"
+      cpu : 256,
+      memoryReservation : 512,
+      essential : true,
+      name : "my-nginx",
+      image : "${aws_ecr_repository.nginx.repository_url}:latest"
     }
   ])
   family = "my-nginx"
@@ -86,15 +109,15 @@ resource "aws_default_route_table" "public" {
 }
 
 resource "aws_route" "public" {
-  route_table_id = aws_default_route_table.public.id
-  gateway_id = aws_internet_gateway.nginx.id
+  route_table_id         = aws_default_route_table.public.id
+  gateway_id             = aws_internet_gateway.nginx.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
 resource "aws_subnet" "public-subnet" {
-  cidr_block = "10.0.10.0/24"
-  availability_zone = "ap-northeast-1a"
-  vpc_id = aws_vpc.nginx.id
+  cidr_block              = "10.0.10.0/24"
+  availability_zone       = "ap-northeast-1a"
+  vpc_id                  = aws_vpc.nginx.id
   map_public_ip_on_launch = true
   tags = {
     Name = "nginx-public"
@@ -103,41 +126,41 @@ resource "aws_subnet" "public-subnet" {
 
 resource "aws_security_group" "nginx-sg" {
   vpc_id = aws_vpc.nginx.id
-  name = "nginx-sg"
+  name   = "nginx-sg"
   ingress {
-    from_port = 80
-    protocol = "TCP"
-    to_port = 80
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-//resource "aws_ecs_service" "nginx" {
-//  name = "nginx-ecs"
-//  cluster = aws_ecs_cluster.nginx.arn
-//  task_definition = "my-nginx:1"
-//  desired_count = 1
-//
-//  launch_type = "FARGATE"
-//  deployment_minimum_healthy_percent = 100
-//  deployment_maximum_percent = 200
-//  network_configuration {
-//    subnets = [
-//      aws_subnet.public-subnet.id
-//    ]
-//
-//    security_groups = [
-//      aws_security_group.nginx-sg.id
-//    ]
-//
-//    assign_public_ip = true
-//  }
-//
-//}
+resource "aws_ecs_service" "nginx" {
+  name            = "nginx-ecs"
+  cluster         = aws_ecs_cluster.nginx.arn
+  task_definition = "my-nginx:8"
+  desired_count   = 1
+
+  launch_type                        = "FARGATE"
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  network_configuration {
+    subnets = [
+      aws_subnet.public-subnet.id
+    ]
+
+    security_groups = [
+      aws_security_group.nginx-sg.id
+    ]
+
+    assign_public_ip = true
+  }
+
+}
